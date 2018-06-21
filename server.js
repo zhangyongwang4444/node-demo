c0f57b912dba2801bd38bed06f22d417f81742e4
zhangyongwang <349368992@qq.com>
2018-06-21 15:37:32 +0800

    Node.js服务器

--- server.js
+++ server.js
@@ -32,21 +32,21 @@ var server = http.createServer(function(request, response){
 
 
   console.log('方方说：得到 HTTP 路径\n' + path)
-  if(path == '/style'){
+  if(path == '/style.css'){
     response.setHeader('Content-Type','text/css; charset=utf-8')
     response.write('body{background-color:#ddd;}h1{color:red;}')
     response.end()
-  }else if(path == '/script'){
+  }else if(path == '/main.js'){
     response.setHeader('Content-Type','text/javascript; charset=utf-8')
     response.write('alert("这是JS执行的")')
     response.end()
-  }else if(path == '/index'){
+  }else if(path == '/'){
     response.setHeader('Content-Type','text/html; charset=utf-8')
     response.write('<!DOCTYPE/>\n<html>'+
-    '<head><link rel="stylesheet" href="/style">'+
+    '<head><link rel="stylesheet" href="/style.css">'+
     '</head><body>'+
     '<h1>你好</h1>'+
-    '<script src="/script"></script>'+
+    '<script src="/main.js"></script>'+
     '</body></html>')
     response.end()
   }else{
